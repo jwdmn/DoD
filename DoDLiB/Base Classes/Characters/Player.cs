18f248eef6fbe2e611c8823aa9a11c374b0ece87
ACADEMY-7115W6B\Administrator <wadmanjonas@gmail.com>
2017-07-05 15:00:28 +0200

    Added XML-comments

--- DoDLiB/Base Classes/Characters/Player.cs
+++ DoDLiB/Base Classes/Characters/Player.cs
@@ -6,12 +6,21 @@ using System.Threading.Tasks;
 
 namespace DoD
 {
+    /// <summary>
+    /// Represents a player in the game.
+    /// </summary>
     public class Player : Character, IAbleToPickup
     {
         public int X { get; set; }
         public int Y { get; set; }
         public List<IPickupAble> Backpack { get; set; }
 
+        /// <summary>
+        /// Represents a player in the game.
+        /// </summary>
+        /// <param name="x"> Represents the players X-coordinate on the world map</param>
+        /// <param name="y">Represents the players Y-coordinate on the world map</param>
+        /// <param name="name">Represents the players name</param>
         public Player(int x, int y, string name) : base(50, 5, 'P', name)
         {
             X = x;
--- Utils/RandomUtils.cs
+++ Utils/RandomUtils.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace Utils
 {
+    /// <summary>
+    /// Utils class for randomizing
+    /// </summary>
     public static class RandomUtils
     {
         static Random random = new Random();
 
+        /// <summary>
+        /// Rolls for specified winchance
+        /// </summary>
+        /// <param name="winPercentage">percent win chance</param>
+        /// <returns></returns>
         public static bool RandomRoll(int winPercentage)
         {
             int result = random.Next(1, 101);
